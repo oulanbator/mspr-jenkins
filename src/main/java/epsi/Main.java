e6272854e382fa26663c7fdb796fd46a9b424a84
Victor Matheron <victor.matheron@cgi.com>
2022-02-02 17:39:54 +0100

    Rebuild start

--- src/main/java/epsi/Main.java
+++ src/main/java/epsi/Main.java
@@ -1,28 +1,37 @@
 package epsi;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Retriever retriever = new Retriever();
-        retriever.deletePreviousHTML(new File("src/newSite/agents"));
-        try {
-            retriever.setMaterials();
 
-            List<Agent> agents = retriever.tratAllagentes();
-            for(Agent agent : agents){
-                Generator.setFiche(agent, retriever.materialsMap);
-                retriever.uploadIDimage(agent);
-            }
-            Generator.setIndex(agents);
-            Generator.setHtaccess(agents);
-            Generator.setHtpassword(agents);
+        FileReader.print("agents/durand.txt");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        Retriever retriever = new Retriever();
+//        retriever.deletePreviousHTML(new File("src/newSite/agents"));
+//        try {
+//            retriever.setMaterials();
+//
+//            List<Agent> agents = retriever.tratAllagentes();
+//            for(Agent agent : agents){
+//                Generator.setFiche(agent, retriever.materialsMap);
+//                retriever.uploadIDimage(agent);
+//            }
+//            Generator.setIndex(agents);
+//            Generator.setHtaccess(agents);
+//            Generator.setHtpassword(agents);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
+
 }
 
