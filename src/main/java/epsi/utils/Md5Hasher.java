ca5dce1b3740c470cb7746c21039bc3bb341e1ef
Victor Matheron <victor.matheron@cgi.com>
2022-02-07 12:34:42 +0100

    Fix htpasswd generation

--- src/main/java/epsi/utils/Md5Hasher.java
+++ src/main/java/epsi/utils/Md5Hasher.java
@@ -1,16 +1,15 @@
 package epsi.utils;
 
 import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Md5Hasher {
 
     public static String getHash(String user, String password)
             throws NoSuchAlgorithmException, UnsupportedEncodingException  {
-        String realm = "Fiche agent protégée, veuillez vous identifier";
+        String realm = "gosecuri";
 
-        byte b[] = java.security.MessageDigest.getInstance("MD5").digest( (user + ":" + realm + ":" + password ).getBytes());
+        byte b[] = java.security.MessageDigest.getInstance("MD5").digest( (user + ":" + realm + ":" + password).getBytes());
         java.math.BigInteger bi = new java.math.BigInteger(1, b);
         String s = bi.toString(16);
         while (s.length() < 32)
